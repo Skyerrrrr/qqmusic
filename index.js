d85c4a6eeff227253bcd89aeacd0c206e058f1fd
Skyerrrrr <95363552+Skyerrrrr@users.noreply.github.com>
2022-02-27 12:42:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+/*window.onload = function(){
+  var imgList = document.getElementById("sl_img_ul");
+  var circle = document.getElementsByClassName("circle");
+  var index = 0;
+  for(var i=0; i<circle.length; i++){
+   
+    circle[i].addEventListener('click',function(){
+      i = index;
+      imgList.style.left = -1220*index + "px";
+    });
+  }
+}
+*/
+/*
+let oLeft = document.querySelector(".sl_btn_left");
+let oRight = document.querySelector(".sl_btn_right");
+let oImgList = document.querySelector(".sl_img_ul");
+
+oRight.addEventListener("click", () =>{
+  index++;
+ oImgList.getElementsByClassName.left = index * -1220 + "px";
+});
+*/
